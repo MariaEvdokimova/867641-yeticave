fc25d00b94711fd400724719e6d6613b4b29a6a7
MariaEvdokimova <e.marrri@mail.ru>
2019-02-09 11:40:54 +0300

    Изменено условие в процедуре: что бы знак рубля добавлялся к цене <1000

--- index.php
+++ index.php
@@ -49,8 +49,8 @@ function formatting_price($value)
     $value = ceil($value);
     if ($value >= 1000){
         $value = number_format($value, 0, '',' ');
-        $value .= ' &#8381;';
     }
+    $value .= ' &#8381;';
     return $value;
 }
 
