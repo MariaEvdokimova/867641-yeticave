d4b083b5a7db0734eeb896d4df82139b6e74c17c
MariaEvdokimova <e.marrri@mail.ru>
2019-03-11 00:00:34 +0300

    Изменено количество вводимых символов

--- page_content/sign-up.php
+++ page_content/sign-up.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     validate_img('avatar', $errors);
     validate_email($sign, 'email', $errors, $link);
     validate_str_len($sign['name'], $errors, 'name', 128);
-    validate_str_len($sign['contacts'], $errors, 'contacts', 65535);
+    validate_str_len($sign['contacts'], $errors, 'contacts', 300);
 
     if (count($errors) == 0) {
         $sign['password'] = password_hash($sign['password'], PASSWORD_DEFAULT);
